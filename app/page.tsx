87e20e0903df670462f46c38a09548a1fc4c51e9
fujinuma1937 <117887185+fujinuma1937@users.noreply.github.com>
2024-03-19 18:00:47 +0900

    編集機能を追加した

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Image from "next/image";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 type Todo = {
-  id: number;
+  id: string;
   title: string;
   edit: boolean;
   deadline: string;
@@ -13,18 +13,31 @@ type Todo = {
 export default function Home() {
   const [todoList, settodoList] = useState<Todo[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
-  const [isEdit, setIsEdit] = useState<boolean[]>([]);
+  const [editArray, setEditArray] = useState<boolean[]>([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingText, setEditingText] = useState("");
+  const [editingDate, setEditingDate] = useState("");
   const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "";
+
+  const generateID = () => {
+    const length = 12;
+    const strings = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let newID = "";
+    for (let i = 0; i < length; i++) {
+      newID += strings[Math.floor(Math.random() * strings.length)];
+    }
+    return newID;
+  };
 
   const getAllData = async () => {
     await axios
       .get(API_URL)
       .then((res) => {
-        const newEdit = []
+        const newEdit = [];
         for (let i = 0; i < res.data.length; i++) {
           newEdit[i] = false;
         }
-        setIsEdit(newEdit);
+        setEditArray(newEdit);
         settodoList([...res.data]);
       })
       .catch(() => {
@@ -36,19 +49,23 @@ export default function Home() {
     if (inputValue) {
       await axios
         .post(API_URL, {
+          id: generateID(),
           title: inputValue,
-          delete: false,
+          deadline: false,
         })
         .then(() => {
           getAllData();
           setInputValue("");
+        })
+        .catch(() => {
+          alert("追加に失敗しました");
         });
     } else {
       alert("入力してください");
     }
   };
 
-  const deleteData = async (id: any) => {
+  const deleteData = async (id: string) => {
     const payload = {
       id: id,
     };
@@ -62,12 +79,39 @@ export default function Home() {
       });
   };
 
-  const editStart = (index: number) => {
-    setIsEdit(isEdit.map((edit, number) => (number === index ? !edit : edit)));
+  const updateData = async (id: string) => {
+    const payload = {
+      id: id,
+      title: editingText,
+      deadline: editingDate,
+    };
+    await axios
+      .put(`${API_URL}/${id}`, payload)
+      .then(() => {
+        getAllData();
+      })
+      .catch(() => {
+        alert("編集に失敗しました");
+      });
   };
 
-  const editEnd = (index: number) => {
-    setIsEdit(isEdit.map((edit, number) => (number === index ? !edit : edit)));
+  const editStart = (index: number) => {
+    if (!isEditing) {
+      setIsEditing((prev) => !prev);
+      setEditArray(
+        editArray.map((edit, number) => (number === index ? !edit : edit))
+      );
+      setEditingText(todoList[index].title);
+      setEditingDate(todoList[index].deadline);
+    }
+  };
+
+  const editEnd = (index: number, id: string) => {
+    setIsEditing((prev) => !prev);
+    setEditArray(
+      editArray.map((edit, number) => (number === index ? !edit : edit))
+    );
+    updateData(id);
   };
 
   useEffect(() => {
@@ -76,19 +120,20 @@ export default function Home() {
 
   return (
     <main className="">
-      {isEdit}
+      {editArray}
       <section className="bg-indigo-200 w-fit p-11 m-auto my-10">
         <ul className="flex flex-col gap-y-5 mb-10">
           {todoList.map((todo, index) => {
             return (
               <li className="" key={todo.id}>
                 <div className="flex gap-x-2">
-                  {isEdit[index] ? (
+                  {editArray[index] ? (
                     <input
                       className="min-w-96 bg-cyan-100 w-fit flex items-center px-2"
                       type="text"
                       defaultValue={todo.title}
-                      placeholder="入力しやがれ"
+                      placeholder="入力してください"
+                      onChange={(event) => setEditingText(event.target.value)}
                     />
                   ) : (
                     <p className="min-w-96 bg-white w-fit flex items-center px-2">
@@ -96,11 +141,12 @@ export default function Home() {
                     </p>
                   )}
 
-                  {isEdit[index] ? (
+                  {editArray[index] ? (
                     <input
                       type="date"
                       value={todo.deadline}
                       className="bg-cyan-100  w-fit flex items-center px-2"
+                      onChange={(event) => setEditingDate(event.target.value)}
                     />
                   ) : (
                     <p className="bg-white w-fit flex items-center px-2">
@@ -108,10 +154,10 @@ export default function Home() {
                     </p>
                   )}
 
-                  {isEdit[index] ? (
+                  {editArray[index] ? (
                     <button
                       className="bg-stone-600 text-white p-2 shadow-md"
-                      onClick={() => editEnd(index)}
+                      onClick={() => editEnd(index, todo.id)}
                     >
                       編集完了
                     </button>
